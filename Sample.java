d28687fdb922c22cb742cd2b23f29c6975c36c55
ishikawa <ishikawahrzf@pm.nttdata-ccs.co.jp>
2019-12-24 11:23:21 +0900

    java

---
+++ Sample.java
@@ -0,0 +1,5 @@
+public class Sample {
+	public int calc(int a, int b) {
+		return a + b;
+	}
+}
---
+++ SampleTest.java
@@ -0,0 +1,7 @@
+@RunWith(Sample.class)
+public class SampleTest {
+    @Test
+    public void index() throws Exception {
+        assertTrue(false);
+    }
+}
